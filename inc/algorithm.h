6959b59b04c2ab0917c9e580518519c58cab796a
Megan Cater <mcater@nd.edu>
2021-04-24 19:03:25 -0400

    Updated insert_data and algorithms files

--- inc/algorithm.h
+++ inc/algorithm.h
@@ -3,15 +3,28 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <cstdlib>
+#include <string>
 
-#define OFSTREAM std::ofstream
+#include "DLL.h"
+#include "Hash_Table.h"
+
+#define IFSTREAM std::ifstream
 #define OSTREAM std::ostream
 #define CIN std::cin
 #define COUT std::cout
+#define STRING std::string
+#define GETLINE std::getline
+#define STOF std::stof
+
+/* Data structures enumerated list */
+enum Data_Structure { dllist, array, hashtable };
+
 
 /* Inserts data from text file stream into specified text file
  * Returns true if data was entered successfully and false if memory threshold is exceeded */
-bool insert_data(OFSTREAM& stream, bool isconstant, int data_structure, const long unsigned int memorydata);
+bool insert_data(IFSTREAM& input, bool isconstant, int data_structure, const long unsigned int memorydata);
 
 /* Switches to a more optimal data structure */
 void switch_data_structure(int current_data_structure);
@@ -22,4 +35,4 @@ int run_duplicate_check(OFSTREAM& stream, int duplicate_threshold);
 /* Determines the total amount of memory the data structure will take up */
 long unsigned int determine_total_memory(int count, int data_structure);
 
-#endif ALGORITHM_H
+#endif
--- src/algorithm.cpp
+++ src/algorithm.cpp
@@ -2,7 +2,46 @@
 
 /* Inserts data from text file stream into specified text file
  * Returns true if data was entered successfully and false if memory threshold is exceeded */
-bool insert_data(OFSTREAM& stream, bool isconstant, int data_structure, const long unsigned int memorydata) {
+bool insert_data(IFSTREAM& input, bool isconstant, int data_structure, const long unsigned int memorydata) {
+	STRING line;
+	int count = 0;
+
+	// Data structure objects
+	DLL dll;
+	float arr[___];
+	HashTable hash;
+
+	// Amount of data is not changing
+	if (isconstant) {
+		// Reads data until EOF is reached
+		while (GETLINE(input, line)) {
+			// Converts string line of input to float
+			float data = STOF(line);
+
+			count++;
+			
+			// Inserts data into doubly linked list
+			if (data_structure == dllist) {
+				dll.insert(data);
+			}
+
+			// Inserts data into array
+			if (data_structure == array) {
+				// ?
+			}
+
+			// Inserts data into hash table
+			if (data_structure == hashtable) {
+				hash.insert(__);
+			}
+
+			// Checks if memory threshold has been exceeded
+			if (determine_total_memory(count, data_structure) > memorydata) {
+				switch_data_structure(data_structure);
+			}
+		}
+	}
+
 	return true;
 }
 
